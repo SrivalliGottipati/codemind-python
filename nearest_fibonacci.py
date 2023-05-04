25328ea492161b0327b511d1a01a19137289353e
SrivalliGottipati <121477373+SrivalliGottipati@users.noreply.github.com>
2023-05-04 22:11:19 +0530

    n=int(input()) a,b=0,1 while(True):     c=a+b     if(c>n):        h1=b        h2=c        break     a=b     b=c #print(h1,h2) x1=abs(h1-n) x2=abs(h2-n) if(x1==x2):    print(h1,h2) elif(x1>x2):    print(h2) else:    print(h1)

---
+++ nearest_fibonacci.py
@@ -0,0 +1,20 @@
+n=int(input())
+a,b=0,1
+while(True):
+    c=a+b
+    if(c>n):
+       h1=b
+       h2=c
+       break
+    a=b
+    b=c
+#print(h1,h2)
+x1=abs(h1-n)
+x2=abs(h2-n)
+if(x1==x2):
+   print(h1,h2)
+elif(x1>x2):
+   print(h2)
+else:
+   print(h1)
+   
