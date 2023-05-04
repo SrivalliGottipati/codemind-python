01898d493731f29e5803fa97f0481c113088022d
SrivalliGottipati <121477373+SrivalliGottipati@users.noreply.github.com>
2023-05-04 22:07:56 +0530

    def compute_lcm(x, y):    if x > y:        greater = x    else:        greater = y
       while(True):
           if((greater % x == 0) and (greater % y == 0)):
               lcm = greater
               break
           greater += 1
    
       return lcm
    
    num1 ,num2=map(int,input().split())
    
    print( compute_lcm(num1, num2))

---
+++ LCM.py
@@ -0,0 +1,17 @@
+def compute_lcm(x, y):
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+
+num1 ,num2=map(int,input().split())
+
+print( compute_lcm(num1, num2))
