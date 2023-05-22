fb2e723fbe73773c9afb8fd8d5db229e06372b7e
SrivalliGottipati <121477373+SrivalliGottipati@users.noreply.github.com>
2023-05-22 20:27:39 +0530

    t=int(input()) for i in range(1,t+1):     x=int(input())     a=list(map(int,input().split()))     c=0     for i in a:         if i%2==1:             c+=1     print(c//2)

--- Weird_Palindrome_Making.py
+++ Weird_Palindrome_Making.py
@@ -1,10 +1,9 @@
 t=int(input())
-for i in range(t):
-    c=0
-    n=int(input())
+for i in range(1,t+1):
+    x=int(input())
     a=list(map(int,input().split()))
-    s=len(a)
-    for j in range(0,s):
-        if a[j]%2!=0:
+    c=0
+    for i in a:
+        if i%2==1:
             c+=1
     print(c//2)
